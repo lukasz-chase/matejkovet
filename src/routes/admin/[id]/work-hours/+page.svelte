0dbd538548938279d9c02ee3d0585e4c6e4fdcad
Łukasz Ścigaj <lukasz7522@gmail.com>
2023-08-16 22:45:46 +0200

    styling working hours

--- src/routes/admin/[id]/work-hours/+page.svelte
+++ src/routes/admin/[id]/work-hours/+page.svelte
@@ -18,25 +18,29 @@
     class="flex flex-col items-center justify-center"
   >
     {#each Object.keys(data.workingHours) as day}
-      <div class="flex items-center mb-2">
-        <label for={day} class="w-24">{day}:</label>
-        <input
-          step="3600"
-          type="time"
-          name={day + "-open"}
-          id={day + "-open"}
-          bind:value={data.workingHours[day].open}
-          class="input input-bordered mr-2"
-        />
-        to
-        <input
-          step="3600"
-          type="time"
-          name={day + "-close"}
-          id={day + "-close"}
-          bind:value={data.workingHours[day].close}
-          class="input input-bordered ml-2"
-        />
+      <div
+        class="flex flex-col md:flex-row items-center md:justify-center mb-2 text-sm md:text-xl"
+      >
+        <label for={day} class="w-full md:w-36">{day}:</label>
+        <div>
+          <input
+            step="3600"
+            type="time"
+            name={day + "-open"}
+            id={day + "-open"}
+            bind:value={data.workingHours[day].open}
+            class="input input-bordered mr-2"
+          />
+          do
+          <input
+            step="3600"
+            type="time"
+            name={day + "-close"}
+            id={day + "-close"}
+            bind:value={data.workingHours[day].close}
+            class="input input-bordered ml-2"
+          />
+        </div>
       </div>
     {/each}
     <button class="btn btn-primary btn-wide mt-4">Zapisz</button>
